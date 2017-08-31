d4869f7fd185c443afe2b5474b93db8bab4bf3ae
loren pabst <lorenpabst@gmail.com>
2017-08-30 20:24:14 -0600

    fixing the alerts when the game is a tie

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -11,19 +11,32 @@ angular.module("app")
     $scope.player2 = 'Human';
 
     $scope.nextTurn = function(){
-        let player = $scope.turn;
-        if ($scope[player] === 'Computer'){
-            let coordinates = $scope.getComputerMove();
-            if ($scope.turn === 'player1'){
-                $scope.board[coordinates[0]][coordinates[1]] = 'x'
-                $scope.turn = 'player2';
-                checkWinner()
-            }else{
-                $scope.board[coordinates[0]][coordinates[1]] = 'o'
-                $scope.turn = 'player1';
-                checkWinner();
+        if ($scope.getValidMove()){
+            let player = $scope.turn;
+            if ($scope[player] === 'Computer'){
+                var coordinates = $scope.getValidMove();
+                setTimeout(function(){
+                    if ($scope.turn === 'player1'){
+                        $scope.board[coordinates[0]][coordinates[1]] = 'x'
+                        $scope.$apply();
+                        if (checkWinner()){
+                            return alertWinner($scope.turn)
+                        }
+                        $scope.turn = 'player2';
+                    }else{
+                        $scope.board[coordinates[0]][coordinates[1]] = 'o'
+                        $scope.$apply();
+                        if (checkWinner()){
+                            return alertWinner($scope.turn)
+                        }
+                        $scope.turn = 'player1';
+                    }
+                    $scope.nextTurn();
+                }, 500)
             }
-        }
+        }else{
+            return alertWinner(false)
+        }        
     }
 
     $scope.chooseSquare = function(arr){
@@ -36,80 +49,87 @@ angular.module("app")
             }else{
                 if ($scope.turn === 'player1'){
                     $scope.board[arr[0]][arr[1]] = 'x'
+                    if (checkWinner()){
+                        return alertWinner($scope.turn)
+                    }
                     $scope.turn = 'player2'
                 }else if ($scope.turn === 'player2'){
                     $scope.board[arr[0]][arr[1]] = 'o'
+                    if (checkWinner()){
+                        return alertWinner($scope.turn)
+                    }
                     $scope.turn = 'player1'
                 }
-                checkWinner()
                 $scope.nextTurn()
             }
         }
     }
 
-    $scope.getComputerMove = function(){
-        let row = Math.floor(Math.random() * 3)
-        let column = Math.floor(Math.random() * 3)
-        if ($scope.board[row][column] !== ''){
-            while ($scope.board[row][column] !== ''){
-                row = Math.floor(Math.random() * 3)
-                column = Math.floor(Math.random() * 3)
+    $scope.getValidMove = function(){
+        let board = $scope.board;
+        for (var i = 0; i < 3; i ++){
+            if (board[0][i] === '' || board[1][i] === '' || board[2][i] === ''){
+                // console.log('found empty square');
+                if (board[1][1] === ''){
+                    return [1, 1]
+                }else{
+                    // for (var i = 0; i < 3; i++){
+                    //     for (var j = 0; j < 3; j ++){
+
+                    //     }
+                    // }
+                    let row = Math.floor(Math.random() * 3)
+                    let column = Math.floor(Math.random() * 3)
+                    if (board[row][column] !== ''){
+                        while (board[row][column] !== ''){
+                            row = Math.floor(Math.random() * 3)
+                            column = Math.floor(Math.random() * 3)
+                        }
+                    }
+                    return [row, column]
+                }
+            }else{
+                // console.log('no empty squares on row ' + i)
             }
         }
-        return [row, column]
+        // console.log('no empty squares at all')
+        return false;
     }
 
     function checkWinner(){
+        var board = $scope.board;
         for (var i = 0; i < 2; i ++){
             //horizontal
-            if ($scope.board[i][0] === 'x' && $scope.board[i][1] === 'x' && $scope.board[i][2] === 'x'){
-                setTimeout(function(){
-                    alert('X won');
-                    $scope.resetBoard();
-                }, 20)
-            }else if ($scope.board[i][0] === 'o' && $scope.board[i][1] === 'o' && $scope.board[i][2] === 'o'){
-                setTimeout(function(){
-                    alert('O won');
-                    $scope.resetBoard();
-                }, 20)
+            if (board[i][0] === 'x' && board[i][1] === 'x' && board[i][2] === 'x' || board[i][0] === 'o' && board[i][1] === 'o' && board[i][2] === 'o'){
+                return true;
             }
             //vertical
-            else if ($scope.board[0][i] === 'o' && $scope.board[1][i] === 'o' && $scope.board[2][i] === 'o'){
-                setTimeout(function(){
-                    alert('O won');
-                    $scope.resetBoard();
-                }, 20)
-            }else if ($scope.board[0][i] === 'x' && $scope.board[1][i] === 'x' && $scope.board[2][i] === 'x'){
-                setTimeout(function(){
-                    alert('X won');
-                    $scope.resetBoard();
-                }, 20)
+            else if (board[0][i] === 'o' && board[1][i] === 'o' && board[2][i] === 'o' || board[0][i] === 'x' && board[1][i] === 'x' && board[2][i] === 'x'){
+                return true;
             }
             //top left to bottom right diagonal
-            else if ($scope.board[0][0] === 'o' && $scope.board[1][1] === 'o' && $scope.board[2][2] === 'o'){
-                setTimeout(function(){
-                    alert('O won');
-                    $scope.resetBoard();
-                }, 20)
-            }else if ($scope.board[0][0] === 'x' && $scope.board[1][1] === 'x' && $scope.board[2][2] === 'x'){
-                setTimeout(function(){
-                    alert('X won');
-                    $scope.resetBoard();
-                }, 20)
+            else if (board[0][0] === 'o' && board[1][1] === 'o' && board[2][2] === 'o' || board[0][0] === 'x' && board[1][1] === 'x' && board[2][2] === 'x'){
+                return true;
             }
             //top right to bottom left diagonal
-            else if ($scope.board[2][0] === 'o' && $scope.board[1][1] === 'o' && $scope.board[0][2] === 'o'){
-                setTimeout(function(){
-                    alert('O won');
-                    $scope.resetBoard();
-                }, 20)
-            }else if ($scope.board[2][0] === 'x' && $scope.board[1][1] === 'x' && $scope.board[0][2] === 'x'){
-                setTimeout(function(){
-                    alert('X won');
-                    $scope.resetBoard();
-                }, 20)
+            else if (board[2][0] === 'o' && board[1][1] === 'o' && board[0][2] === 'o' || board[2][0] === 'x' && board[1][1] === 'x' && board[0][2] === 'x'){
+                return true;
             }
         }
+        return false;
+    }
+
+    function alertWinner(player){
+        if (!player){
+            var message = 'The game is a tie!'
+        }else{
+            var message = 'Player ' + player.substring(6) + ' wins!'
+        }
+        setTimeout(function(){
+            alert(message);
+            $scope.resetBoard();
+            $scope.$apply();
+        }, 20)
     }
 
     $scope.resetBoard = function(){
@@ -118,7 +138,9 @@ angular.module("app")
             ['', '', ''],
             ['', '', '']
         ]
+        $scope.nextTurn();
     }
+
 
 
 });
